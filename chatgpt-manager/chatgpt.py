38c30456d5903eb5b3936b0494015df77e038653
dhwndud <wndud5577@somma.kr>
2023-05-04 13:53:46 +0900

    test. chatgpt+slack 연동 테스트 코드 추가

---
+++ chatgpt-manager/chatgpt.py
@@ -0,0 +1,31 @@
+import openai
+
+# 발급받은 OpenAI API Key 기입
+YOUR_API_KEY = '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'
+
+
+def ChatGPT(prompt, API_KEY=YOUR_API_KEY):
+    # api key 세팅
+    openai.api_key = API_KEY
+
+    # ChatGPT API 호출 및 최신 언어 모델인 text-davinci-003을 가져옴
+    completion = openai.Completion.create(
+        engine='text-davinci-003'  # 'text-curie-001'  # 'text-babbage-001' #'text-ada-001'
+        , prompt=prompt
+        , temperature=0.5
+        , max_tokens=1024
+        , top_p=1
+        , frequency_penalty=0
+        , presence_penalty=0)
+
+    return completion['choices'][0]['text']
+
+
+def main():
+    # 지문 입력 란
+    prompt = input("Insert a prompt: ")
+    print(ChatGPT(prompt).strip())
+
+
+if __name__ == '__main__':
+    main()
